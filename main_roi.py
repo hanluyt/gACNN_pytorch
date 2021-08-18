74bd27948c99d7fa0f8c402fb2820e7e09c18f56
hanluyt <72287017+hanluyt@users.noreply.github.com>
2021-08-18 17:19:24 +0800

    new version

---
+++ main_roi.py
@@ -0,0 +1,247 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function
+import logging
+import os
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from model_roi import *
+import argparse
+import numpy as np
+from tqdm import tqdm
+import random
+import pandas as pd
+import time
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+logger = logging.getLogger(__name__)
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val):
+        self.val = val
+        self.sum += val
+        self.count += 1
+        self.avg = self.sum / self.count
+
+def simple_accuracy(preds, labels):
+    return (preds == labels).mean()  # np.array
+
+def save_model(args, model):
+    model_to_save = model.module if hasattr(model, 'module') else model
+    model_checkpoint = os.path.join(args.output_dir, "%s_roi_cpu.pth" % args.name)
+    torch.save(model_to_save.state_dict(), model_checkpoint)
+    logger.info("Saved model checkpoint to [DIR: %s]", args.output_dir)
+
+def count_parameters(model):
+    params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return params/1000000
+
+def set_seed(args):
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+def setup(args):
+    model = ACNN()
+    model.to(args.device)
+    num_params = count_parameters(model)
+
+    logger.info("Training parameters %s", args)
+    logger.info("Total Parameter: \t%2.1fM" % num_params)
+    print(num_params)
+    return args, model
+
+def valid(args, model, test_loader, global_step):
+    # Validation
+    eval_losses = AverageMeter()
+
+    logger.info("***** Running Validation *****")
+    logger.info(" Num steps = %d", len(test_loader))
+    logger.info(" Batch size = %d", args.batchsize)
+
+    model.eval()
+    all_preds, all_label = [], []
+    epoch_iterator = tqdm(test_loader,
+                          desc="Validating... (loss=X.X)",
+                          bar_format="{l_bar}{r_bar}",
+                          dynamic_ncols=True)
+    loss_fct = nn.CrossEntropyLoss()
+    for step, batch in enumerate(epoch_iterator):
+        imgs, landmarks, targets = batch['image'], batch['landmarks'] / 8. + 6, batch['label']
+        landmarks = roi_select(landmarks)
+        imgs, landmarks, targets = imgs.to(args.device), landmarks.to(args.device), targets.to(args.device)
+        with torch.no_grad():
+            logits = model(imgs, landmarks)
+            eval_loss = loss_fct(logits, targets)
+            eval_losses.update(eval_loss.item())
+            preds = torch.argmax(logits, dim=-1)
+
+        if len(all_preds) == 0:
+            all_preds.append(preds.detach().cpu().numpy())
+            all_label.append(targets.detach().cpu().numpy())
+        else:
+            all_preds[0] = np.append(all_preds[0], preds.detach().cpu().numpy(), axis=0)
+            all_label[0] = np.append(all_label[0], targets.detach().cpu().numpy(), axis=0)
+        epoch_iterator.set_description("Validating... (loss=%2.5f)" % eval_losses.val)
+
+    all_preds, all_label = all_preds[0], all_label[0]
+    accuracy = simple_accuracy(all_preds, all_label)
+
+    logger.info("\n")
+    logger.info("Validation Results")
+    logger.info("Global epochs: %d" % global_step)
+    logger.info("Valid Loss: %2.5f" % eval_losses.avg)
+    logger.info("Valid Accuracy: %2.5f" % accuracy)
+
+    return accuracy, eval_losses.avg
+
+def train(args, model):
+    """ Train the model """
+    # Prepare dataset
+    shuffle = True
+    train_set = FaceLandmarksDataset(csv_file='util/RAF-DB_train_24.csv', root_dir='RAF-DB_train/',
+                                     transform=transforms.Compose([Rescale((224, 224)), ToTensor()]))
+    test_set = FaceLandmarksDataset(csv_file='util/RAF-DB_test_24.csv', root_dir='RAF-DB_test/',
+                                    transform=transforms.Compose([Rescale((224, 224)), ToTensor()]))
+
+    train_loader = DataLoader(dataset=train_set, shuffle=shuffle, batch_size=args.batchsize, num_workers=4, pin_memory=True)
+    test_loader = DataLoader(dataset=test_set, shuffle=shuffle, batch_size=args.batchsize, num_workers=4, pin_memory=True)
+
+    # Prepare optimizer and scheduler
+    optimizer = torch.optim.SGD(model.parameters(),
+                                lr=args.learning_rate,
+                                momentum=0.9,
+                                weight_decay=args.weight_decay)
+    t_total = args.num_steps
+    # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [int(t_total/10), int(t_total/2)],gamma=0.1, last_epoch=-1)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=5, eta_min=0)
+    loss_fct = nn.CrossEntropyLoss()
+    # Train!
+    logger.info("***** Running training *****")
+    logger.info("  Total optimization epochs = %d", args.num_steps)
+    logger.info("  batch size = %d", args.batchsize)
+
+    model.zero_grad()
+    # set_seed(args)  # Added here for reproducibility
+    global_epoch, best_acc = 0, 0
+    train_losses = AverageMeter()
+    # train_preds, train_label = [], []
+    total_train_acc, total_train_loss, total_test_acc, total_test_loss = [], [], [], []
+    while True:
+        model.train()
+        train_preds, train_label = [], []
+        epoch_iterator = tqdm(train_loader,
+                              desc="Training (X / X epochs) (loss=X.X)",
+                              bar_format="{l_bar}{r_bar}",
+                              dynamic_ncols=True)
+        for step, batch in enumerate(epoch_iterator):
+            imgs, landmarks, targets = batch['image'], batch['landmarks'] / 8. + 6, batch['label']
+            landmarks = roi_select(landmarks)
+            imgs, landmarks, targets = imgs.to(args.device), landmarks.to(args.device), targets.to(args.device)
+            logits = model(imgs, landmarks)
+            train_loss = loss_fct(logits, targets)
+            train_losses.update(train_loss.item())
+            preds = torch.argmax(logits, dim=-1)
+            optimizer.zero_grad()
+            if len(train_preds) == 0:
+                train_preds.append(preds.detach().cpu().numpy())
+                train_label.append(targets.detach().cpu().numpy())
+            else:
+                train_preds[0] = np.append(train_preds[0], preds.detach().cpu().numpy(), axis=0)
+                train_label[0] = np.append(train_label[0], targets.detach().cpu().numpy(), axis=0)
+
+            epoch_iterator.set_description(
+                "Training (%d / %d epochs) (loss=%2.5f)" % (global_epoch+1, t_total, train_losses.val))
+            train_loss.backward()
+            scheduler.step()
+            optimizer.step()
+
+        global_epoch += 1
+
+        train_preds, train_label = train_preds[0], train_label[0]
+        train_acc = simple_accuracy(train_preds, train_label)
+        train_loss = train_losses.avg
+        logger.info("Train Loss: %2.5f" % train_loss)
+        logger.info("Train Accuracy: %2.5f" % train_acc)
+        total_train_acc.append(train_acc), total_train_loss.append(train_loss)
+
+        test_acc, test_loss = valid(args, model, test_loader, global_epoch)
+        total_test_acc.append(test_acc), total_test_loss.append(test_loss)
+        if best_acc < test_acc:
+            save_model(args, model)
+            best_acc = test_acc
+            model.train()
+
+        train_losses.reset()
+        if global_epoch == t_total:
+            break
+
+    logger.info("Best Accuracy: \t%f" % best_acc)
+    logger.info("End Training!")
+    train_metric = [[acc, loss] for acc, loss in zip(total_train_acc, total_train_loss)]
+    test_metric = [[acc, loss] for acc, loss in zip(total_test_acc, total_test_loss)]
+    train_metric = pd.DataFrame(train_metric, columns=['train_acc', 'train_loss'])
+    test_metric = pd.DataFrame(test_metric, columns=['test_acc', 'test_loss'])
+    train_metric.to_csv('train_acnn.csv', index=False)
+    test_metric.to_csv('test_acnn.csv', index=False)
+
+def main():
+    parser = argparse.ArgumentParser()
+    # Required parameters
+    parser.add_argument("--name", required=True,
+                        help="Name of this run. Used for monitoring.")
+
+    parser.add_argument("--output_dir", default="acnn_result", type=str,
+                        help="The output directory where checkpoints will be written.")
+
+    parser.add_argument("--batchsize", default=128, type=int,
+                        help="Total batch size for training.")
+
+    parser.add_argument("--learning_rate", default=1e-1, type=float,
+                        help="The initial learning rate for SGD.")
+
+    parser.add_argument("--weight_decay", default=5e-4, type=float,
+                        help="Weight deay if we apply some.")
+
+    parser.add_argument("--num_steps", default=600, type=int,
+                        help="Total number of training epochs to perform.")
+
+    parser.add_argument('--seed', type=int, default=42,
+                        help="random seed for initialization")
+
+    args = parser.parse_args()
+    # args.name = 'acnn'
+    # args.batchsize = 8
+    # args.learning_rate = 3e-2
+    # args.seed = 42
+    # args.num_steps = 10
+    # args.output_dir = 'acc_result'
+    # args.weight_decay = 0
+    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Setup logging
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+                        datefmt='%m/%d/%Y %H:%M:%S', level=logging.INFO)
+
+    # Set seed
+    # set_seed(args)
+
+    # Model & Tokenizer Setup
+    args, model = setup(args)
+
+    # Training
+    train(args, model)
+
+
+if __name__ == "__main__":
+    main()
---
+++ model_roi.py
@@ -0,0 +1,153 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+from util.custom_dataset import FaceLandmarksDataset, Rescale, ToTensor
+import torchvision.models as models
+from torchvision import transforms
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision import transforms, utils
+import torchvision
+
+class View(nn.Module):
+    def __init__(self, shape):
+        super(View, self).__init__()
+        self.shape = shape
+
+    def forward(self, x):
+        return x.view(*self.shape)
+
+class InnerSum(nn.Module):
+    def __init__(self):
+        super(InnerSum, self).__init__()
+    def forward(self, x):
+        y = torch.zeros_like(x)
+        for i in range(x.size(0)):
+            y[i] = x[i].mul(x[i])
+        if len(y.shape) == 3:
+            return y.sum(2)
+        else:
+            return y.sum(1)
+
+class ACNN(nn.Module):
+    def __init__(self):
+        super(ACNN, self).__init__()
+        self.inner = InnerSum()
+        self.pretrain = models.vgg16(pretrained=True).features[:28]
+        self.VGG16 = self.pretrain[:21]
+        self.PG_base = nn.Sequential(nn.Conv2d(512, 512, kernel_size=3, padding=1), nn.BatchNorm2d(512), nn.ReLU(),
+                                     nn.Conv2d(512, 512, kernel_size=3, padding=1), nn.BatchNorm2d(512), nn.ReLU(),)
+        self.PG_attention = nn.Sequential(nn.MaxPool2d(2, stride=2), nn.Conv2d(512, 128, kernel_size=3, padding=1),
+                                          nn.BatchNorm2d(128), nn.ReLU(), nn.AdaptiveAvgPool2d((1, 1)), View((-1, 128)),
+                                          nn.Linear(128, 64), nn.ReLU(), nn.Linear(64, 1), nn.Sigmoid())
+        self.GG_base = self.pretrain[21:]
+        self.GG_attention = nn.Sequential(nn.MaxPool2d(2, stride=2), nn.Conv2d(512, 128, kernel_size=3, padding=1),
+                                          nn.BatchNorm2d(128), nn.ReLU(), nn.AdaptiveAvgPool2d((1, 1)), View((-1, 128)),
+                                          nn.Linear(128, 64), nn.ReLU(), nn.Linear(64, 1), nn.Sigmoid())
+        self.PG24_base = nn.ModuleList([self.PG_base for _ in range(24)])
+        self.PG24_alpha = nn.ModuleList([self.PG_attention for _ in range(24)])
+
+        self.pad = nn.ReflectionPad2d(6)
+        self.crop = torchvision.ops.roi_pool
+        self.PG_fc = nn.Linear(512*6*6, 64)
+        self.GG_fc = nn.Linear(512*14*14, 512)
+        self.fc1 = nn.Linear(2048, 1024)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(1024, 7)
+
+    # def crop_layer(self, img: '(B, C, H, W)', landmarks: '(B, 24, 2)'):
+    #     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #     pad = nn.ReflectionPad2d(6)  # padding for cropping
+    #     img = pad(img)  # (B, 512, 36, 36)
+    #     total_crop = torch.zeros((img.size(0), landmarks.size(1), 512, 6, 6), device=self.device)
+    #
+    #     for i in range(landmarks.size(0)):  # Batch
+    #         # crop_per_batch = []
+    #         for patch in range(landmarks.size(1)):  # 24 landmarks
+    #             total_crop[i, patch, :, :, :] = img[i, :, (int(landmarks[i, patch, 0]) - 3): (int(
+    #                 landmarks[i, patch, 0]) + 3),
+    #                                             (int(landmarks[i, patch, 1]) - 3): (int(
+    #                                                 landmarks[i, patch, 1]) + 3)]  # crop_img: (512, 6, 6)
+    #
+    #     total_crop = total_crop.permute(1, 0, 2, 3, 4)  # output: (24, B, 512, 6, 6)
+    #     return total_crop
+
+    def _branch24(self, crop_img):
+        PG_out = []
+        for x, base, alpha in zip(crop_img, self.PG24_base, self.PG24_alpha):
+            PG_conv2 = base(x)
+            PG_reshape = PG_conv2.view(-1, 512*6*6)
+            PG_reshape = self.PG_fc(PG_reshape)
+            PG_per = PG_reshape * alpha(PG_conv2).view(x.size(0), 1)
+            PG_out.append(PG_per)
+        return PG_out
+
+    def forward(self, img, landmarks):
+        img_feature = self.VGG16(img)  # (B, 512, 28, 28)
+        img_pad = self.pad(img_feature)
+        crop_img = self.crop(img_pad, landmarks, output_size=6)
+        crop_img = crop_img.view(24, -1, 512, 6, 6)
+
+        GG_conv2 = self.GG_base(img_feature)
+        GG_reshape = GG_conv2.view(-1, 512*14*14)
+        GG_reshape = self.GG_fc(GG_reshape)
+        GG_out = GG_reshape * self.GG_attention(GG_conv2).view(img_feature.size(0), 1)
+
+        # crop_img = self.crop_layer(img_feature, landmarks)
+        PG_out = self._branch24(crop_img)
+        PG_total = torch.cat(PG_out, dim=1)
+        total_out = torch.cat([GG_out, PG_total], dim=1)
+
+        out = self.fc1(total_out)
+        out = F.relu(self.dropout(out))
+        out = self.fc2(out)
+        return out
+
+def landmark_resize(landmarks:'(B, 24, 2)')->'(B*24, 4)':
+    bs = landmarks.size(0)
+    batch = list(range(bs))
+    batch = np.array(batch * 24).reshape(24, -1).T
+
+    point = np.array(list(range(24)) * bs).reshape(bs, -1)
+    insert_point = np.insert(landmarks, 0, point, 2)
+    insert_batch = np.insert(insert_point, 0, batch, 2)
+    new_landmark = insert_batch.reshape(-1, 4)
+    return new_landmark
+
+def data_normal(origin_data, size):  # (-1, 1)
+    size = size / 2
+    norm_data = origin_data.true_divide(size) - 1
+    return norm_data
+
+
+def grid_field(landmarks, cropsize=6):  # landmarks: (B, 24, 2)
+    total_crop = []
+    landmarks = landmark_resize(landmarks)  # (B*24, 4)
+    lm_batch = landmarks[:, 0].long()
+    landmarks_x_l = landmarks[:, 2] - (cropsize / 2)
+    landmarks_x_r = landmarks[:, 2] + (cropsize / 2)
+    landmarks_y_l = landmarks[:, 3] - (cropsize / 2)
+    landmarks_y_r = landmarks[:, 3] + (cropsize / 2)
+
+    for i in range(landmarks.size(0)):
+        new_h = torch.linspace(landmarks_x_l[i], landmarks_x_r[i] - 1, cropsize).view(-1, 1).repeat(1, cropsize)
+        new_w = torch.linspace(landmarks_y_l[i], landmarks_y_r[i] - 1, cropsize).repeat(cropsize, 1)
+        grid = torch.cat((new_w.unsqueeze(2), new_h.unsqueeze(2)), dim=2)
+        grid = grid.unsqueeze(0)
+        grid = data_normal(grid, size=28)
+        total_crop.append(grid)
+    total_crop = torch.cat(total_crop, dim=0)
+    return lm_batch, total_crop
+
+def roi_select(landmarks: '(B, 4, 2)')-> '(B*24, 5)':
+    landmarks = landmark_resize(landmarks)
+    landmarks_right = landmarks[:, 2:] + 3
+    landmarks_left = landmarks[:, 2:] - 3
+    landmarks = torch.cat([landmarks[:, 0].view(-1, 1), landmarks_left, landmarks_right], dim=1)
+
+    return landmarks
+
+
